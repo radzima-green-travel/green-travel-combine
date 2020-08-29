8dd95c1ea3602bb0a76ca52b1b322e53ebdc1969
sergey <s.glebko@arateg.com>
2020-08-29 14:35:08 +0300

    fix ultimate config on Android side

--- android/app/src/main/java/com/greentravel/MainApplication.java
+++ android/app/src/main/java/com/greentravel/MainApplication.java
@@ -1,5 +1,4 @@
 package com.greentravel;
-
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -10,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.reactnativeultimateconfig.UltimateConfigModule;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -45,6 +45,7 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    UltimateConfigModule.setBuildConfig(BuildConfig.class);
   }
 
   /**
