0a90bd4c7e575881514f5255226d6b455d12fb2f
sergey <s.glebko@arateg.com>
2021-06-16 09:47:04 +0300

    test xml representation of the icon

--- src/components/screens/Home/screenOptions.tsx
+++ src/components/screens/Home/screenOptions.tsx
@@ -1,7 +1,15 @@
-import {Icon} from 'atoms';
 import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import {IProps, ScreenOptions} from './types';
+
+import {SvgXml} from 'react-native-svg';
+
+const xml = `
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M20.9999 20.9999L16.6499 16.6499" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
 
 const HeaderRight = ({navigation}: IProps) => {
   return (
@@ -9,7 +17,7 @@ const HeaderRight = ({navigation}: IProps) => {
       hitSlop={{top: 15, left: 15, right: 15, bottom: 10}}
       activeOpacity={0.8}
       onPress={() => navigation.navigate('Search')}>
-      <Icon name="searchLight" width={24} height={24} />
+      <SvgXml xml={xml} width={24} height={24} />
     </TouchableOpacity>
   );
 };
