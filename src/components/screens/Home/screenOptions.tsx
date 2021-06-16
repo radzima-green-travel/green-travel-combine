f1fa52c0ce8c70c3541efc23c735b897194a4636
sergey <s.glebko@arateg.com>
2021-06-16 10:42:16 +0300

    header icon test

--- src/components/screens/Home/screenOptions.tsx
+++ src/components/screens/Home/screenOptions.tsx
@@ -2,14 +2,8 @@ import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import {IProps, ScreenOptions} from './types';
 
-import {SvgXml} from 'react-native-svg';
-
-const xml = `
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M20.9999 20.9999L16.6499 16.6499" stroke="#fff" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-`;
+import {Icon} from 'atoms';
+import {COLORS} from 'assets';
 
 const HeaderRight = ({navigation}: IProps) => {
   return (
@@ -17,7 +11,12 @@ const HeaderRight = ({navigation}: IProps) => {
       hitSlop={{top: 15, left: 15, right: 15, bottom: 10}}
       activeOpacity={0.8}
       onPress={() => navigation.navigate('Search')}>
-      <SvgXml xml={xml} width={24} height={24} />
+      <Icon
+        additionalColor={COLORS.white}
+        name={'marker'}
+        color={COLORS.white}
+        size={24}
+      />
     </TouchableOpacity>
   );
 };
