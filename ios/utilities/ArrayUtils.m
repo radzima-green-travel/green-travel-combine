ac619d03fbc553bbd77aa31ab40567e8addeb5e8
Alexey Komov <alexeykcontact@gmail.com>
2021-07-23 20:02:57 +0300

    fix:do not shuffle categories in PlacesCollectionViewCell

--- ios/utilities/ArrayUtils.m
+++ ios/utilities/ArrayUtils.m
@@ -13,5 +13,11 @@ NSArray* shuffledArray(NSArray *items) {
   for (NSUInteger counter = [shuffledItems count]; counter > 1; counter--) {
     [shuffledItems exchangeObjectAtIndex:counter - 1 withObjectAtIndex:arc4random_uniform((u_int32_t)counter)];
   }
-  return [shuffledItems subarrayWithRange:NSMakeRange(0, MIN(10, [items count]))];
+  return [shuffledItems subarrayWithRange:NSMakeRange(0, MIN(10, [shuffledItems count]))];
 }
+
+NSArray* slice(NSArray *items) {
+  NSMutableArray *itemsToSlice = [NSMutableArray arrayWithArray:items];
+  return [itemsToSlice subarrayWithRange:NSMakeRange(0, MIN(10, [itemsToSlice count]))];
+}
+
