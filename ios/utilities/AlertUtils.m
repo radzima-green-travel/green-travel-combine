a869cb20094d901a5b6c51c0b862b541aa1070d9
Alexey Komov <alexeykcontact@gmail.com>
2021-07-23 20:17:30 +0300

    fix:localize geolocation alert

--- ios/utilities/AlertUtils.m
+++ ios/utilities/AlertUtils.m
@@ -8,10 +8,10 @@
 #import "AlertUtils.h"
 
 void showAlertGoToSettings(UIViewController *presenter) {
-  UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Доступ к местоположению"
-                                                                 message:@"Разрешите доступ к местоположению в настройках"
+  UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Доступ к Службам геолокации"
+                                                                 message:@"Если разрешить доступ к Службам геолокации в настройках, Вам будет доступно прокладывание маршрута, центрирование карты на Вашем местоположении"
                                                                  preferredStyle:UIAlertControllerStyleAlert];
-  [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action){}]];
+  [alert addAction:[UIAlertAction actionWithTitle:UIAlertAction style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action){}]];
   [alert addAction:[UIAlertAction actionWithTitle:@"Settings" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString] options:@{} completionHandler:^(BOOL success) {}];
   }]];
