fc67e2b230b89ce3e4658892d2414e9f30021435
alexeykomov <alexeykofficial@gmail.com>
2021-08-14 18:02:33 +0300

    fix:set showLocation ivar

--- ios/screens/MapViewController/MapViewState/MapViewState.m
+++ ios/screens/MapViewController/MapViewState/MapViewState.m
@@ -9,29 +9,34 @@
 
 @interface MapViewState()
 
+@property (assign, nonatomic) double zoomLevel;
+@property (assign, nonatomic) CLLocationCoordinate2D center;
+
 @end
 
 @implementation MapViewState
 
-- (void)saveWithMapView:(MGLMapView *)mapView {
+- (void)saveFromMapView:(MGLMapView *)mapView {
   [self setZoomLevel:mapView.zoomLevel];
   [self setCenter:mapView.centerCoordinate];
-  [self setShowLocation:mapView.showsUserLocation];
-  self.saved = YES;
+  self.saved = self.saved | MapViewStateSaveOptionZoomAndCenter;
+}
+
+- (void)setShowLocation:(BOOL)showLocation {
+  _showLocation = showLocation;
+  self.saved = self.saved | MapViewStateSaveOptionLocation;
 }
 
 - (void)restoreToMap:(MGLMapView *)mapView {
-  if (!self.saved) {
-    return;
+  if (self.saved & MapViewStateSaveOptionLocation) {
+    [mapView setShowsUserLocation:self.showLocation];
+    [mapView setShowsUserHeadingIndicator:self.showLocation];
+    [mapView updateUserLocationAnnotationView];
   }
-  
-  [mapView setZoomLevel:self.zoomLevel];
-  [mapView setCenterCoordinate:self.center];
-  [mapView setShowsUserLocation:NO];
-  [mapView setShowsUserLocation:self.showLocation];
-  [mapView setShowsUserHeadingIndicator:NO];
-  [mapView setShowsUserHeadingIndicator:self.showLocation];
-  [mapView updateUserLocationAnnotationView];
+  if (self.saved & MapViewStateSaveOptionZoomAndCenter) {
+    [mapView setZoomLevel:self.zoomLevel];
+    [mapView setCenterCoordinate:self.center];
+  }
 }
 
 @end
