84d841c18e1e65198ccd2d3c709a4a7d5a0f1e09
Aliaksei Astafyeu <lochost40@gmail.com>
2021-09-29 13:39:03 +0300

    #164: add support of dynamic bottom menu height

--- src/core/hooks/useBottomMenu.ts
+++ src/core/hooks/useBottomMenu.ts
@@ -1,10 +1,11 @@
 import {useCallback, useRef} from 'react';
 import {IBottomMenuRef} from 'atoms/BottomMenu/BottomMenu';
 import {useSharedValue} from 'react-native-reanimated';
+import {SCREEN_HEIGHT} from 'services/PlatformService';
 
 export function useBottomMenu() {
   const menuRef = useRef<IBottomMenuRef>(null);
-  const animatedPosition = useSharedValue(-1);
+  const animatedPosition = useSharedValue(SCREEN_HEIGHT);
 
   const openMenu = useCallback(() => {
     menuRef.current?.show();
