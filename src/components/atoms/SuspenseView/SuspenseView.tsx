f6aed637e7d66bfd6e1aa2acca41f8e99eeaaf2a
Aliaksei Astafyeu <lochost40@gmail.com>
2021-10-25 14:48:12 +0300

    T-111: get rid of redundant hardwareBackPress handler on loading

--- src/components/atoms/SuspenseView/SuspenseView.tsx
+++ src/components/atoms/SuspenseView/SuspenseView.tsx
@@ -1,26 +1,10 @@
-import React, {memo, useEffect} from 'react';
-import {BackHandler} from 'react-native';
+import React, {memo} from 'react';
 import {ErrorView} from 'molecules';
 import {LoadingView} from '../LoadingView';
 import {Props} from './types';
 
 export const SuspenseView = memo<Props>(
-  ({
-    loading,
-    error,
-    retryCallback,
-    children,
-    cover = false,
-    blockBackPress = false,
-  }: Props) => {
-    useEffect(() => {
-      const handler = () => loading || blockBackPress;
-      BackHandler.addEventListener('hardwareBackPress', handler);
-      return () => {
-        BackHandler.removeEventListener('hardwareBackPress', handler);
-      };
-    }, [blockBackPress, loading]);
-
+  ({loading, error, retryCallback, children, cover = false}: Props) => {
     if (error) {
       return <ErrorView onTryAgainPress={retryCallback} error={error} />;
     }
