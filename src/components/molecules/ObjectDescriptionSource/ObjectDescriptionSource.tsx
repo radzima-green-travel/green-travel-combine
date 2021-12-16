3b62b54798877bb82758d0239d695332257b6580
Artsiom Fando <artsiomfando@gmail.com>
2021-12-16 16:37:48 +0300

    Refactor: add separate field for source-info at ObjectDetail-screen

---
+++ src/components/molecules/ObjectDescriptionSource/ObjectDescriptionSource.tsx
@@ -0,0 +1,40 @@
+import React, {memo, useMemo} from 'react';
+import {View, Text, Linking, TouchableOpacity} from 'react-native';
+import {useTranslation} from 'core/hooks';
+import {IOrigins} from 'core/types';
+import {useThemeStyles} from 'core/hooks';
+import {themeStyles} from './styles';
+interface IProps {
+  origins: IOrigins[] | null;
+}
+
+export const ObjectDescriptionSource = memo(({origins}: IProps) => {
+  const styles = useThemeStyles(themeStyles, {disableStyleSheet: true});
+  const {t} = useTranslation('objectDetails');
+
+  const sourceTitle = useMemo(() => {
+    if (!origins?.length) {
+      return null;
+    }
+
+    return origins.length === 1 ? t('source') : t('sources');
+  }, [origins, t]);
+
+  const sourceData = origins?.map(origin => {
+    return (
+      <TouchableOpacity
+        key={origin.name}
+        activeOpacity={0.8}
+        onPress={() => Linking.openURL(origin.value)}>
+        <Text style={styles.link}>{origin.name}</Text>
+      </TouchableOpacity>
+    );
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{sourceTitle}</Text>
+      {sourceData}
+    </View>
+  );
+});
