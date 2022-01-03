18a609f2d841591a00cde86e6f717229f31b4da8
Alexey Komov <alexeykcontact@gmail.com>
2022-01-03 17:03:12 +0300

    [34]fix:no official site button

---
+++ ios/utilities/URLUtils.h
@@ -0,0 +1,11 @@
+//
+//  ImageService.h
+//  GreenTravel
+//
+//  Created by Alex K on 8/28/20.
+//  Copyright © 2020 Alex K. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSString* encodeURL(NSString *url);
