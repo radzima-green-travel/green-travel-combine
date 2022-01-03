71233461be6fb59996f0a9977b6fd6000e58857a
Denis Avramenko <adenis124@gmail.com>
2022-01-03 18:21:32 +0300

    [RN] #187: Added language service to get app language

---
+++ src/services/LanguageService.ts
@@ -0,0 +1,30 @@
+import {NativeModules, Platform} from 'react-native';
+
+import * as RNLocalize from 'react-native-localize';
+import i18n from 'i18next';
+
+class LanguageService {
+  /**
+   * Get all available files with translations via i18n.languages
+   * and find best available language via findBestAvailableLanguage()
+   * 'findBestAvailableLanguage' payed attention to
+   * selected preferred language in app settings:
+   * Device lang: 'en', app lang: 'ru' - will return 'ru'
+   *
+   * @returns {string}
+   */
+  public getPreferredLanguage(): string {
+    const languagesList = i18n.languages;
+
+    const preferredLang = RNLocalize.findBestAvailableLanguage(languagesList);
+
+    const deviceLang =
+      Platform.OS === 'ios'
+        ? NativeModules.SettingsManager.settings.AppleLocale
+        : NativeModules.I18nManager.localeIdentifier;
+
+    return preferredLang?.languageTag || deviceLang;
+  }
+}
+
+export const languageService = new LanguageService();
