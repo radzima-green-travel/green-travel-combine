c05f38fd6d7fd023c9fb0f0b60214c7053882677
Denis Avramenko <adenis124@gmail.com>
2022-01-31 19:25:12 +0300

    One more rebase 996852c2

--- src/services/LanguageService.ts
+++ src/services/LanguageService.ts
@@ -1,54 +1,22 @@
 import {NativeModules, Platform} from 'react-native';
 
 import * as RNLocalize from 'react-native-localize';
-import {initReactI18next} from 'react-i18next';
 import i18n from 'i18next';
 
-import ruTranslations from '../locale/ru.json';
-import enTranslations from '../locale/en.json';
-import zhTranslations from '../locale/zh.json';
-
-import {SupportedLocales} from 'core/types';
-
-/**
- * NOTE: Add language code to SupportedLocales type
- * before adding new language
- */
-const RESOURCES = new Map([
-  ['ru' as const, ruTranslations],
-  ['en' as const, enTranslations],
-  ['zh' as const, zhTranslations],
-]);
-
 class LanguageService {
-  private initialData: object;
-
   /**
-   * Constructor
-   */
-  constructor() {
-    const resources = Object.fromEntries(RESOURCES);
-    this.initialData = {
-      resources,
-      lng: 'ru',
-      fallbackLng: 'ru',
-      interpolation: {
-        escapeValue: false,
-      },
-    };
-  }
-
-  /**
-   * Get all available files with translations via Object.keys(this.#resources)
+   * Get all available files with translations via i18n.languages
    * and find best available language via findBestAvailableLanguage()
    * 'findBestAvailableLanguage' payed attention to
    * selected preferred language in app settings:
    * Device lang: 'en', app lang: 'ru' - will return 'ru'
+   *
+   * @returns {string}
    */
   public getPreferredLanguage(): string {
-    const preferredLang = RNLocalize.findBestAvailableLanguage([
-      ...RESOURCES.keys(),
-    ]);
+    const languagesList = i18n.languages;
+
+    const preferredLang = RNLocalize.findBestAvailableLanguage(languagesList);
 
     const deviceLang =
       Platform.OS === 'ios'
@@ -56,24 +24,6 @@ class LanguageService {
         : NativeModules.I18nManager.localeIdentifier;
 
     return preferredLang?.languageTag || deviceLang;
-  }
-
-  /**
-   * Changes the language of the application
-   */
-  public changeAppLanguage(lang: SupportedLocales): void {
-    i18n.changeLanguage(lang);
-  }
-
-  /**
-   * Init language service
-   */
-  public init(): void {
-    i18n.use(initReactI18next).init(this.initialData);
-
-    const language = this.getPreferredLanguage();
-
-    i18n.changeLanguage(language);
   }
 }
 
