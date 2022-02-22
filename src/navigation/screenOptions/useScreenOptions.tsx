eabf6987a99eeb1c9964ef0672a4b623f290f8bd
Denis Avramenko <adenis124@gmail.com>
2022-02-23 00:41:37 +0300

    [RN] Fix styles

--- src/navigation/screenOptions/useScreenOptions.tsx
+++ src/navigation/screenOptions/useScreenOptions.tsx
@@ -71,18 +71,9 @@ export function useScreenOptions(): NativeStackNavigationOptions {
           headerTitleStyle={{
             ...FONTS_STYLES.semibold16,
           }}
-          headerTitleContainerStyle={{
-            flex: 4,
-            alignItems: 'center',
-          }}
-          headerLeftContainerStyle={{
-            flex: 1,
-            paddingLeft: 16,
-          }}
-          headerRightContainerStyle={{
-            flex: 1,
-            paddingRight: 16,
-          }}
+          headerTitleContainerStyle={{maxWidth: SCREEN_WIDTH * 0.8}}
+          headerLeftContainerStyle={{paddingLeft: 16}}
+          headerRightContainerStyle={{paddingRight: 16}}
           title={titleText}
           {...restOptions}
         />
