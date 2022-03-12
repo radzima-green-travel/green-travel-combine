9c6d87c551385327c62a327caf7d9fe7148063e8
pro-frontend <dzmitryj.ivchenkov@gmail.com>
2022-03-12 19:48:05 +0300

    add query with language parameter as functuin

--- src/api/graphql/customQueries.ts
+++ src/api/graphql/customQueries.ts
@@ -1,3 +1,5 @@
+// import {NativeModules} from 'react-native';
+
 export const listMobileMetadata = /* GraphQL */ `
   query ListMobileData {
     listMobileData {
@@ -97,3 +99,94 @@ export const listMobileData = /* GraphQL */ `
     }
   }
 `;
+
+export const getListMobileData = () => {
+  return /* GraphQL */ `
+    query ListMobileData($limit: Int, $nextToken: String) {
+      listMobileData {
+        categories {
+          id
+          name
+          singularName
+          icon
+          createdAt
+          updatedAt
+          owner
+          createdBy
+          updatedBy
+          fields
+          parent
+          cover
+          index
+          i18n {
+            locale
+            name
+            singularName
+          }
+        }
+        objects(limit: $limit, nextToken: $nextToken) {
+          items {
+            id
+            name
+            description
+            images
+            status
+            location {
+              lat
+              lon
+            }
+            area {
+              type
+              coordinates
+            }
+            cover
+            categoryId
+            owner
+            createdBy
+            updatedBy
+            createdAt
+            updatedAt
+            statusUpdatedAt
+            point {
+              type
+              coordinates
+            }
+            author
+            address
+            length
+            duration
+            origins {
+              name
+              value
+            }
+            notes
+            url
+            routes {
+              type
+              coordinates
+            }
+            include
+            belongsTo
+            i18n {
+              locale
+              name
+              description
+              author
+              address
+              notes
+            }
+          }
+          nextToken
+          total
+        }
+        metadata {
+          id
+          value
+          createdAt
+          updatedAt
+          owner
+        }
+      }
+    }
+  `;
+};
