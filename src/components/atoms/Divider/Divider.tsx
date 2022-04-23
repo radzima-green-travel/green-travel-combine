782c59fffc9512e8ac39cd51f5e747b31d100d62
Artsiom Fando <artsiomfando@gmail.com>
2022-04-23 08:22:26 +0300

    [RN-138]: put Divider-line in a separate component

---
+++ src/components/atoms/Divider/Divider.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from './styles';
+import {useTranslation} from 'core/hooks';
+
+interface IProps {
+  text: string;
+  marginVertical: number;
+}
+
+export const Divider = ({text, marginVertical}: IProps) => {
+  const {t} = useTranslation('authentification');
+
+  return (
+    <View style={[styles.textContainer, {marginVertical: marginVertical}]}>
+      <View style={styles.lineAround} />
+      <Text style={styles.text}>{t(text)}</Text>
+      <View style={styles.lineAround} />
+    </View>
+  );
+};
