bf2dce3ec6d655339ba9c6850fc337508ceb4d47
vitali5070 <naborovski@ya.ru>
2022-06-14 00:56:09 +0300

    fix(402): rebase

--- ios/screens/MapViewController/ItemDetailsMapViewController/ItemDetailsMapViewController.m
+++ ios/screens/MapViewController/ItemDetailsMapViewController/ItemDetailsMapViewController.m
@@ -10,6 +10,7 @@
 @import Mapbox;
 #import "StyleUtils.h"
 #import "ColorsLegacy.h"
+#import "Colors.h"
 #import "MapModel.h"
 #import "MapItemsObserver.h"
 #import "LocationObserver.h"
@@ -252,7 +253,7 @@ static NSString* const kAttributeType = @"type";
     [style addSource:sourcePath];
 
     MGLLineStyleLayer *pathLayer = [[MGLLineStyleLayer alloc] initWithIdentifier:MapViewControllerPathLayerId source:sourcePath];
-    pathLayer.lineColor = [NSExpression expressionForConstantValue:[ColorsLegacy get].persimmon];
+    pathLayer.lineColor = [NSExpression expressionForConstantValue:[Colors get].areaOutline];
     pathLayer.lineOpacity = [NSExpression expressionForConstantValue:@1];
     pathLayer.lineCap = [NSExpression expressionForConstantValue:@"round"];
     pathLayer.lineWidth =
@@ -264,7 +265,7 @@ static NSString* const kAttributeType = @"type";
     [style addSource:sourceOutline];
 
     MGLLineStyleLayer *outlineLayer = [[MGLLineStyleLayer alloc] initWithIdentifier:MapViewControllerPathLayerId source:sourceOutline];
-    outlineLayer.lineColor = [NSExpression expressionForConstantValue:[ColorsLegacy get].green];
+    outlineLayer.lineColor = [NSExpression expressionForConstantValue:[Colors get].areaOutline];
     outlineLayer.lineOpacity = [NSExpression expressionForConstantValue:@1];
     outlineLayer.lineCap = [NSExpression expressionForConstantValue:@"round"];
     outlineLayer.lineWidth =
@@ -277,9 +278,9 @@ static NSString* const kAttributeType = @"type";
     [style addSource:sourcePolygon];
 
     MGLFillStyleLayer *polygonLayer = [[MGLFillStyleLayer alloc] initWithIdentifier:MapViewControllerPolygonLayerId source:sourcePolygon];
-    polygonLayer.fillColor = [NSExpression expressionForConstantValue:[ColorsLegacy get].green];
+    polygonLayer.fillColor = [NSExpression expressionForConstantValue:[Colors get].areaFill];
     polygonLayer.fillOpacity = [NSExpression expressionForConstantValue:@0.3];
-    polygonLayer.fillOutlineColor = [NSExpression expressionForConstantValue:[ColorsLegacy get].green];
+    polygonLayer.fillOutlineColor = [NSExpression expressionForConstantValue:[Colors get].areaOutline];
 
     [style addLayer:polygonLayer];
   }
