cc5bf94ac81f7eeccf3f9aa88cf4b3d6a115aba6
alexeykomov <alexeykofficial@gmail.com>
2022-06-17 01:33:04 +0300

    add:reset password new password screen

--- ios/screens/ProfileRootViewController/ResetPasswordNewPasswordViewController/ResetPasswordNewPasswordViewController.m
+++ ios/screens/ProfileRootViewController/ResetPasswordNewPasswordViewController/ResetPasswordNewPasswordViewController.m
@@ -6,26 +6,144 @@
 //
 
 #import "ResetPasswordNewPasswordViewController.h"
+#import "SecureTextField.h"
+#import "CommonButton.h"
+#import "Typography.h"
+#import "UserController.h"
+#import "UserModel.h"
+#import "UserModelConstants.h"
+#import "ResetPasswordPassCodeViewController.h"
 
 @interface ResetPasswordNewPasswordViewController ()
+
+@property(strong, nonatomic) UILabel *hintLabel;
+@property(strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) SecureTextField *textFieldNewPassword;
+@property(strong, nonatomic) CommonButton *buttonSubmit;
+@property(assign, nonatomic) BOOL shownKeyboard;
+@property(assign, nonatomic) BOOL navigatedToCodeScreen;
 
 @end
 
 @implementation ResetPasswordNewPasswordViewController
 
+- (void)viewDidLayoutSubviews {
+  [self.hintLabel setPreferredMaxLayoutWidth:self.view.frame.size.width - 47.0];
+}
+
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+  [super viewDidLoad];
+  self.title = NSLocalizedString(@"ResetPasswordEMailScreenTitle", @"");
+  
+  self.titleLabel = [[UILabel alloc] init];
+  NSAttributedString *header = [[Typography get] codeConfirmationHeader:NSLocalizedString(@"ResetPasswordEMailScreenHeader", @"")];
+  [self.titleLabel setAttributedText:header];
+  [self.titleLabel setNumberOfLines:0];
+  [self.titleLabel setTextAlignment:NSTextAlignmentCenter];
+  [self.contentView addSubview:self.titleLabel];
+  self.titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+
+  [NSLayoutConstraint activateConstraints:@[
+    [self.titleLabel.centerXAnchor constraintEqualToAnchor:self.contentView.centerXAnchor],
+    [self.titleLabel.leadingAnchor constraintGreaterThanOrEqualToAnchor:self.contentView.leadingAnchor],
+    [self.titleLabel.trailingAnchor constraintLessThanOrEqualToAnchor:self.contentView.trailingAnchor],
+    [self.titleLabel.topAnchor constraintEqualToAnchor:self.contentView.topAnchor constant:150.0],
+  ]];
+
+  self.hintLabel = [[UILabel alloc] init];
+  NSAttributedString *hint = [[Typography get] codeConfirmationHint:NSLocalizedString(@"ResetPasswordEMailScreenHint", @"")];
+  [self.hintLabel setAttributedText:hint];
+  [self.hintLabel setNumberOfLines:0];
+  [self.hintLabel setTextAlignment:NSTextAlignmentCenter];
+  [self.contentView addSubview:self.hintLabel];
+  self.hintLabel.translatesAutoresizingMaskIntoConstraints = NO;
+
+  [NSLayoutConstraint activateConstraints:@[
+    [self.hintLabel.centerXAnchor constraintEqualToAnchor:self.contentView.centerXAnchor],
+    [self.hintLabel.leadingAnchor constraintGreaterThanOrEqualToAnchor:self.contentView.leadingAnchor],
+    [self.hintLabel.trailingAnchor constraintLessThanOrEqualToAnchor:self.contentView.trailingAnchor],
+    [self.hintLabel.topAnchor constraintEqualToAnchor:self.titleLabel.bottomAnchor constant:20.0],
+  ]];
+  
+  self.textFieldNewPassword = [[SecureTextField alloc] initWithImageName:@"password-lock"
+                                                     keyboardType:UIKeyboardTypeDefault
+                                                      placeholder:NSLocalizedString(@"ProfileScreenPlaceholderPassword", @"")];
+  [self.textFieldNewPassword.textField setTextContentType:UITextContentTypeEmailAddress];
+  [self.contentView addSubview:self.textFieldNewPassword];
+  self.textFieldNewPassword.translatesAutoresizingMaskIntoConstraints = NO;
+  [NSLayoutConstraint activateConstraints:@[
+      [self.textFieldNewPassword.topAnchor constraintEqualToAnchor:self.hintLabel.bottomAnchor constant:20.0],
+      [self.textFieldNewPassword.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor],
+      [self.textFieldNewPassword.trailingAnchor constraintEqualToAnchor:self.contentView.trailingAnchor],
+  ]];
+  
+  self.buttonSubmit = [[CommonButton alloc] initWithTarget:self
+                                                    action:@selector(onSubmit:)
+                                                     label:NSLocalizedString(@"CodeConfirmationScreenSubmit", @"")];
+  self.buttonSubmit.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.contentView addSubview:self.buttonSubmit];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [self.buttonSubmit.centerXAnchor constraintEqualToAnchor:self.contentView.centerXAnchor],
+    [self.buttonSubmit.topAnchor constraintEqualToAnchor:self.textFieldNewPassword.bottomAnchor constant:25.0],
+    [self.buttonSubmit.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor],
+    [self.buttonSubmit.trailingAnchor constraintEqualToAnchor:self.contentView.trailingAnchor],
+    
+  ]];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewDidAppear:(BOOL)animated {
+  if (!self.shownKeyboard) {
+    self.shownKeyboard = YES;
+    [self.textFieldNewPassword becomeFirstResponder];
+  }
 }
-*/
+
+- (void)onUserModelStateTransitionFrom:(UserModelState)prevState
+                  toCurrentState:(UserModelState)currentState {
+  dispatch_async(dispatch_get_global_queue(QOS_CLASS_UTILITY, 0), ^{
+    dispatch_async(dispatch_get_main_queue(), ^{
+      if (prevState == UserModelStateFetched && currentState == UserModelStateSignUpEmailInProgress) {
+        [self enableLoadingIndicator:YES];
+        return;
+      }
+      if (prevState == UserModelStateConfirmCodeNotSent && currentState == UserModelStateConfirmCodeInProgress) {
+        [self enableLoadingIndicator:YES];
+        return;
+      }
+      if (prevState == UserModelStateNotSignedIn && currentState == UserModelStateSignInInProgress) {
+        [self enableLoadingIndicator:YES];
+        return;
+      }
+      
+      if (prevState == UserModelStateSignUpEmailInProgress && currentState == UserModelStateFetched) {
+        [self enableLoadingIndicator:NO];
+        return;
+      }
+      if (prevState == UserModelStateConfirmCodeInProgress && currentState == UserModelStateConfirmCodeSent) {
+        [self enableLoadingIndicator:NO];
+        return;
+      }
+      if (prevState == UserModelStateSignInInProgress && currentState == UserModelStateSignedIn) {
+        // Handler in ProfileRootViewController.
+        return;
+      }
+      
+      
+      if (prevState == UserModelStatePasswordResetConfirmCodeInProgress && currentState == UserModelStatePasswordResetConfirmCodeSet) {
+        return;
+      }
+      if (prevState == UserModelStatePasswordResetConfirmCodeInProgress && currentState == UserModelStatePasswordResetConfirmCodeNotSet) {
+        return;
+      }
+    });
+  });
+}
+
+
+- (void)onSubmit:(CommonButton *)sender {
+  [self.userController initiateResetPassword:self.textFieldNewPassword.textField.text];
+  [self.view endEditing:YES];
+}
 
 @end
