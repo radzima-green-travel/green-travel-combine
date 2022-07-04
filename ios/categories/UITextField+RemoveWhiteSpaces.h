5843bd90a08291b76cc06493c109eee09fdf1358
kudr1kk22 <kudr1kk22@gmail.com>
2022-07-04 20:15:06 +0300

    fix/437 trim whitespaces into textfield

---
+++ ios/categories/UITextField+RemoveWhiteSpaces.h
@@ -0,0 +1,16 @@
+//
+//  UITextField+RemoveWhiteSpaces.h
+//  greenTravel
+//
+//  Created by kudr7k on 4.07.22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UITextField (RemoveWhiteSpaces)
+
+@end
+
+NS_ASSUME_NONNULL_END
