af55979d68189f949a421e6ac86331cd46dff643
alexeykomov <alexeykofficial@gmail.com>
2022-07-10 20:09:51 +0300

    feat(467): [WIP]add index file

---
+++ ios/services/ApiService/ApiServiceIndexFile.h
@@ -0,0 +1,22 @@
+//
+//  IndexFileApiService.h
+//  greenTravel
+//
+//  Created by Alex K on 10.07.22.
+//
+
+#import <Foundation/Foundation.h>
+#import "ApiService.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class DetailsModel;
+@class PlaceCategory;
+@class PlaceDetails;
+
+@interface ApiServiceIndexFile : ApiService
+
+@end
+
+NS_ASSUME_NONNULL_END
+
