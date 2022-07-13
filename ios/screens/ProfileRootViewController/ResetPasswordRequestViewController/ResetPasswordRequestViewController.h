551d016f171fb11cebe7fc550205381b7a323a55
Alexey Komov <alexeykcontact@gmail.com>
2022-07-13 17:33:56 +0300

    feat(465): [WIP]add reset password flow when sign in failed

---
+++ ios/screens/ProfileRootViewController/ResetPasswordRequestViewController/ResetPasswordRequestViewController.h
@@ -0,0 +1,16 @@
+//
+//  ResetPasswordRequestViewController.h
+//  greenTravel
+//
+//  Created by Alex K on 13.07.22.
+//
+
+#import "BaseFormViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ResetPasswordRequestViewController : BaseFormViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
