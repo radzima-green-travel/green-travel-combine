d661e34801df7c8ba8c2b0d5d3d928272e4b9f83
Alexey Komov <alexeykcontact@gmail.com>
2022-07-26 12:24:09 +0300

    fix(464): new password screen can be scrolled

--- ios/screens/ProfileRootViewController/ResetPasswordNewPasswordViewController/ResetPasswordNewPasswordViewController.m
+++ ios/screens/ProfileRootViewController/ResetPasswordNewPasswordViewController/ResetPasswordNewPasswordViewController.m
@@ -92,7 +92,7 @@
     [self.buttonSubmit.topAnchor constraintEqualToAnchor:self.textFieldNewPassword.bottomAnchor constant:25.0],
     [self.buttonSubmit.leadingAnchor constraintEqualToAnchor:self.contentView.leadingAnchor],
     [self.buttonSubmit.trailingAnchor constraintEqualToAnchor:self.contentView.trailingAnchor],
-
+    [self.buttonSubmit.bottomAnchor constraintLessThanOrEqualToAnchor:self.contentView.bottomAnchor constant:CommonFormButtonBottomSpace],
   ]];
 }
 
