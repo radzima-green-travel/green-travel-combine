90a46cf92b700ffc7f2d9ee5655b98b53799044d
Vitali Nabarouski <naborovski@ya.ru>
2022-08-01 21:25:57 +0300

    fix(485): change function for tint icons

--- ios/extensions/UIImage+extensions.m
+++ ios/extensions/UIImage+extensions.m
@@ -10,41 +10,14 @@
 @implementation UIImage (Extension)
 
 - (UIImage *)tintedImage:(UIColor *)color {
-    CGRect rect = CGRectMake(0.f, 0.f, self.size.width, self.size.height);
-    CGImageRef alphaMask;
-
-    {
-        UIGraphicsBeginImageContext(rect.size);
-        CGContextRef context = UIGraphicsGetCurrentContext();
-
-        CGContextTranslateCTM(context, 0, rect.size.height);
-        CGContextScaleCTM(context, 1.0, -1.0);
-        [self drawInRect: rect];
-
-        alphaMask = CGBitmapContextCreateImage(context);
-
-        UIGraphicsEndImageContext();
-    }
-
-    UIGraphicsBeginImageContext(rect.size);
-
-    CGContextRef context = UIGraphicsGetCurrentContext();
-
-    [self drawInRect:rect];
-    CGContextClipToMask(context, rect, alphaMask);
-
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    CGContextSetFillColorSpace(context, colorSpace);
-    CGContextSetFillColorWithColor(context, color.CGColor);
-    UIRectFillUsingBlendMode(rect, kCGBlendModeNormal);
-
-    UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-
-    CGColorSpaceRelease(colorSpace);
-    CGImageRelease(alphaMask);
-
-    return tintedImage;
+  UIGraphicsBeginImageContextWithOptions(self.size, FALSE, self.scale);
+  
+  CGRect drawRect = CGRectMake(0, 0, self.size.width, self.size.height);
+  [color setFill];
+  UIRectFill(drawRect);
+  [self drawInRect:drawRect blendMode:kCGBlendModeDestinationIn alpha:1.0];
+  UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
+  return tintedImage;
 }
 
 @end
