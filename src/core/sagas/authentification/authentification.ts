a9201a455cef56a12fd5e39d73830f92fffb7d17
Aliaksei Astafyeu <lochost40@gmail.com>
2022-09-13 22:42:25 +0200

    fix(52): add feature toggle for my account

--- src/core/sagas/authentification/authentification.ts
+++ src/core/sagas/authentification/authentification.ts
@@ -1,4 +1,4 @@
-import {takeEvery} from 'redux-saga/effects';
+import {select, takeEvery} from 'redux-saga/effects';
 import {ACTIONS} from 'core/constants';
 import {confirmNewPasswordSaga} from './confirmNewPasswordSaga';
 import {confirmSignUpSaga} from './confirmSignUpSaga';
@@ -7,13 +7,23 @@ import {resendSignUpCodeSaga} from './resendSignUpCodeSaga';
 import {signInSaga} from './signInSaga';
 import {signUpSaga} from './signUpSaga';
 import {userAuthorizedSaga} from './userAuthorizedSaga';
+import {selectIsMyProfileFeatureEnabled} from 'core/selectors';
 
 export function* authentificationSaga() {
-  yield takeEvery(ACTIONS.CONFIRM_NEW_PASSWORD_REQUEST, confirmNewPasswordSaga);
-  yield takeEvery(ACTIONS.CONFIRM_SIGNUP_REQUEST, confirmSignUpSaga);
-  yield takeEvery(ACTIONS.FORGOT_PASSWORD_REQUEST, forgotPasswordSaga);
-  yield takeEvery(ACTIONS.RESEND_SIGNUP_CODE_REQUEST, resendSignUpCodeSaga);
-  yield takeEvery(ACTIONS.SIGNIN_REQUEST, signInSaga);
-  yield takeEvery(ACTIONS.SIGNUP_REQUEST, signUpSaga);
-  yield takeEvery(ACTIONS.USER_AUTHORIZED_REQUEST, userAuthorizedSaga);
+  const isMyProfileFeatureEnabled: ReturnType<
+    typeof selectIsMyProfileFeatureEnabled
+  > = yield select(selectIsMyProfileFeatureEnabled);
+
+  if (isMyProfileFeatureEnabled) {
+    yield takeEvery(
+      ACTIONS.CONFIRM_NEW_PASSWORD_REQUEST,
+      confirmNewPasswordSaga,
+    );
+    yield takeEvery(ACTIONS.CONFIRM_SIGNUP_REQUEST, confirmSignUpSaga);
+    yield takeEvery(ACTIONS.FORGOT_PASSWORD_REQUEST, forgotPasswordSaga);
+    yield takeEvery(ACTIONS.RESEND_SIGNUP_CODE_REQUEST, resendSignUpCodeSaga);
+    yield takeEvery(ACTIONS.SIGNIN_REQUEST, signInSaga);
+    yield takeEvery(ACTIONS.SIGNUP_REQUEST, signUpSaga);
+    yield takeEvery(ACTIONS.USER_AUTHORIZED_REQUEST, userAuthorizedSaga);
+  }
 }
