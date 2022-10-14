9c0ac09bd027593df389d24d7d03856545d8b172
Alexey Komov <alexeykcontact@gmail.com>
2022-10-14 20:10:21 +0300

    feat(590): add accessibility ids

---
+++ ios/utilities/AccessibilityIdentifiers.h
@@ -0,0 +1,18 @@
+//
+//  AccessibilityIdentifiers.h
+//  greenTravel
+//
+//  Created by Alex K on 14.10.22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXPORT const NSString* AccessibilityIdentifiersTabBarMain;
+FOUNDATION_EXPORT const NSString* AccessibilityIdentifiersTabBarMap;
+FOUNDATION_EXPORT const NSString* AccessibilityIdentifiersTabBarFavorites;
+FOUNDATION_EXPORT const NSString* AccessibilityIdentifiersTabBarProfile;
+
+
+NS_ASSUME_NONNULL_END
