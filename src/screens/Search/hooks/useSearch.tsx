4f74e4b5feeaf1600385e834c2e5307c459b5443
Vladislav Shevtsov <vladshevtsov56@gmail.com>
2022-11-10 11:17:33 +0100

    feat(updated): add useSearch hook

---
+++ src/screens/Search/hooks/useSearch.tsx
@@ -0,0 +1,56 @@
+import {useCallback, useEffect} from 'react';
+
+import {IObject} from 'core/types';
+import {useSearchList} from 'core/hooks';
+import {Keyboard} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {SearchScreenNavigationProps} from '../types';
+
+export const useSearch = () => {
+  const {
+    data,
+    isHistoryVisible,
+    addToHistory,
+    deleteFromHistory,
+    deleteAllFromHistory,
+    clearInput,
+  } = useSearchList();
+
+  const navigation = useNavigation<SearchScreenNavigationProps>();
+
+  const navigateToObjectDetails = useCallback(
+    (searchItem: IObject) => {
+      Keyboard.dismiss();
+      addToHistory(searchItem);
+      navigation.navigate('ObjectDetails', {
+        objectId: searchItem.id,
+      });
+    },
+    [addToHistory, navigation],
+  );
+
+  const deleteItem = useCallback(
+    (searchItem: IObject) => {
+      deleteFromHistory(searchItem);
+    },
+    [deleteFromHistory],
+  );
+
+  const deleteAllItems = useCallback(() => {
+    deleteAllFromHistory();
+  }, [deleteAllFromHistory]);
+
+  useEffect(() => {
+    return () => {
+      clearInput();
+    };
+  }, [clearInput]);
+
+  return {
+    isHistoryVisible,
+    data,
+    navigateToObjectDetails,
+    deleteItem,
+    deleteAllItems,
+  };
+};
