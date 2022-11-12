1e52e67f0aa40c8849d9e63c979a6bab0495e438
Vladislav Shevtsov <vladshevtsov56@gmail.com>
2022-11-12 16:45:52 +0100

    feat(updated): add useBookMarks & useBookmarksList hooks

--- src/screens/Bookmarks/Bookmarks.tsx
+++ src/screens/Bookmarks/Bookmarks.tsx
@@ -1,42 +1,19 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import {View, ScrollView} from 'react-native';
 import {styles} from './styles';
 import {BookmarkItem, SuspenseView} from 'atoms';
-import {selectBookmarksCardsData} from 'core/selectors';
-import {useSelector, useDispatch} from 'react-redux';
-import {
-  useRequestError,
-  useRequestLoading,
-  useBookmarksAnalytics,
-} from 'core/hooks';
-import {getInitialHomeDataRequest} from 'core/reducers';
-import {IProps} from './types';
-import {IBookmarkItem} from 'core/types';
 import {isEmpty} from 'lodash';
 import {BookmarksEmptyView} from 'molecules';
+import {useBookmarks} from './hooks';
 
-export const Bookmarks = ({navigation}: IProps) => {
-  const bookmarksCategories = useSelector(selectBookmarksCardsData);
-  const dispatch = useDispatch();
-  const {sendSelectSavedCategoryEvent} = useBookmarksAnalytics();
-
-  const getHomeData = useCallback(() => {
-    dispatch(getInitialHomeDataRequest());
-  }, [dispatch]);
-
-  const {loading} = useRequestLoading(getInitialHomeDataRequest);
-  const {error} = useRequestError(getInitialHomeDataRequest);
-
-  const navigateToBookmarksList = useCallback(
-    ({categoryName, categoryId}: IBookmarkItem) => {
-      navigation.navigate('BookmarksList', {
-        title: categoryName,
-        categoryId: categoryId,
-      });
-      sendSelectSavedCategoryEvent(categoryName);
-    },
-    [navigation, sendSelectSavedCategoryEvent],
-  );
+export const Bookmarks = () => {
+  const {
+    getHomeData,
+    bookmarksCategories,
+    loading,
+    error,
+    navigateToBookmarksList,
+  } = useBookmarks();
 
   return (
     <SuspenseView
--- src/screens/BookmarksList/BookmarksList.tsx
+++ src/screens/BookmarksList/BookmarksList.tsx
@@ -2,34 +2,22 @@ import React, {useCallback, useEffect, useLayoutEffect, useMemo} from 'react';
 import {FlatList} from 'react-native';
 import {ObjectCard} from 'molecules';
 import {styles} from './styles';
-import {IProps} from './types';
 
 import {isAndroid, isIOS, SCREEN_WIDTH} from 'services/PlatformService';
 import {PADDING_HORIZONTAL} from 'core/constants';
-import {useBookmarksObjects, useBookmarksListAnalytics} from 'core/hooks';
-import {IObject} from 'core/types';
-import {orderBy} from 'lodash';
+import {useBookmarksList} from './hooks';
 
 const cardWidth = SCREEN_WIDTH - PADDING_HORIZONTAL * 2;
 
-export const BookmarksList = ({
-  route,
-  navigation: {setOptions, goBack, navigate},
-}: IProps) => {
+export const BookmarksList = () => {
   const {
-    params: {title, categoryId},
-  } = route;
-
-  const listData = useBookmarksObjects(categoryId);
-
-  const {sendSaveCardEvent, sendSelectCardEvent, sendUnsaveCardEvent} =
-    useBookmarksListAnalytics();
-
-  const sortedListData = useMemo(() => {
-    return listData
-      ? orderBy(listData, [({name}) => name.toLowerCase()], 'asc')
-      : null;
-  }, [listData]);
+    setOptions,
+    sortedListData,
+    goBack,
+    title,
+    navigateToObjectDetails,
+    sendIsFavoriteChangedEvent,
+  } = useBookmarksList();
 
   useLayoutEffect(() => {
     setOptions({
@@ -48,25 +36,6 @@ export const BookmarksList = ({
       goBack();
     }
   }, [goBack, onLastObjectRemoveAnimationEnd, sortedListData]);
-
-  const navigateToObjectDetails = useCallback(
-    ({id, name, category}: IObject) => {
-      navigate('ObjectDetails', {objectId: id});
-      sendSelectCardEvent(name, category.name);
-    },
-    [navigate, sendSelectCardEvent],
-  );
-
-  const sendIsFavoriteChangedEvent = useCallback(
-    ({name, category}: IObject, nextIsFavoriteStatus: boolean) => {
-      if (nextIsFavoriteStatus) {
-        sendSaveCardEvent(name, category.name);
-      } else {
-        sendUnsaveCardEvent(name, category.name);
-      }
-    },
-    [sendSaveCardEvent, sendUnsaveCardEvent],
-  );
 
   return (
     <FlatList
