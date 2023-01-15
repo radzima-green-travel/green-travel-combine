a98ea3edf4772ea7e1e498aa93bd994b3177f8ff
Nataliia_Yakorieva <Nataliia_Yakorieva@epam.com>
2023-01-15 20:37:51 +0100

    Add testIds

--- src/screens/Home/screenOptions.tsx
+++ src/screens/Home/screenOptions.tsx
@@ -5,6 +5,7 @@ import {IProps, ScreenOptions} from './types';
 import {Icon} from 'atoms';
 import {useThemeStyles} from 'core/hooks';
 import {themeStyles} from './styles';
+import {TestIDs} from 'core/types';
 
 const HeaderRight = ({navigation}: IProps) => {
   const styles = useThemeStyles(themeStyles);
@@ -13,7 +14,12 @@ const HeaderRight = ({navigation}: IProps) => {
       hitSlop={{top: 15, left: 15, right: 15, bottom: 10}}
       activeOpacity={0.8}
       onPress={() => navigation.navigate('Search')}>
-      <Icon name={'search'} style={styles.icon} size={24} />
+      <Icon
+        name={'search'}
+        style={styles.icon}
+        size={24}
+        testID={TestIDs.SearchButton}
+      />
     </TouchableOpacity>
   );
 };
