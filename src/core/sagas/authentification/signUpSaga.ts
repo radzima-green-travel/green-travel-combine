ca4126243efa35c5a3a224880529d4af14da9cb9
Vladislav Shevtsov <70779585+retater56@users.noreply.github.com>
2023-01-16 17:54:42 +0100

    fix(added): [244] add locale to signUp and forgotPasswordSubmit

--- src/core/sagas/authentification/signUpSaga.ts
+++ src/core/sagas/authentification/signUpSaga.ts
@@ -1,10 +1,13 @@
-import {call, put} from 'redux-saga/effects';
+import {call, put, select} from 'redux-saga/effects';
 import {ActionType} from 'typesafe-actions';
 import {Auth} from 'aws-amplify';
 import {signUpRequest, signUpSuccess, signUpFailure} from 'core/reducers';
+import {selectAppLanguage} from 'core/selectors';
 
 export function* signUpSaga({payload}: ActionType<typeof signUpRequest>) {
   try {
+    const locale = yield select(selectAppLanguage);
+
     yield call([Auth, Auth.signUp], {
       username: payload.email,
       password: payload.password,
@@ -12,6 +15,7 @@ export function* signUpSaga({payload}: ActionType<typeof signUpRequest>) {
         email: payload.email,
         name: '',
         family_name: '',
+        'custom:locale': locale,
       },
       autoSignIn: {
         enabled: true,
