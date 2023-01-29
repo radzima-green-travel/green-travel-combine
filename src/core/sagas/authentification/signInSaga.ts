323506e18a979f0bff99f0af6059ca72c9204657
Vladislav Shevtsov <70779585+retater56@users.noreply.github.com>
2023-01-29 16:56:20 +0100

    fix(fixed): [244] add 'custom:locale' to signIn, signUp, changeLanguage

--- src/core/sagas/authentification/signInSaga.ts
+++ src/core/sagas/authentification/signInSaga.ts
@@ -1,19 +1,31 @@
-import {call, put} from 'redux-saga/effects';
+import {call, put, select} from 'redux-saga/effects';
 import {ActionType} from 'typesafe-actions';
 import {amplifyApi} from 'api/amplify';
 
 import {signInRequest, signInSuccess, signInFailure} from 'core/reducers';
 import {CognitoUserWithAttributes} from '../../types';
+import {selectAppLanguage} from 'core/selectors';
+import {Auth} from 'aws-amplify';
 
 export function* signInSaga({
   payload: {email, password},
 }: ActionType<typeof signInRequest>) {
   try {
+    const locale = yield select(selectAppLanguage);
+
     const {attributes}: CognitoUserWithAttributes = yield call(
       [amplifyApi, amplifyApi.signIn],
       email,
       password,
     );
+
+    if (attributes.email_verified) {
+      const user = yield call([Auth, Auth.currentAuthenticatedUser]);
+
+      yield call([Auth, Auth.updateUserAttributes], user, {
+        'custom:locale': locale,
+      });
+    }
 
     yield put(signInSuccess({email, userAttributes: attributes}));
   } catch (e) {
