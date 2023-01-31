5c147bdd8d9d7ca441f2a7e0aefd11751e3f0ec4
Alexey Komov <alexeykcontact@gmail.com>
2023-01-31 17:00:06 +0300

    feat(638): [WIP] add auth cell

---
+++ ios/screens/SettingsViewController/SettingsAuthTableViewCell/SettingsAuthTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  SettingsAuthTableViewCell.h
+//  greenTravel
+//
+//  Created by Alex K on 31.01.23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsAuthTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
