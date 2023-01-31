c8eefb7330d14ec9c7d094178288d84e6123e33a
Alexey Komov <alexeykcontact@gmail.com>
2023-01-31 17:00:06 +0300

    feat(638): [WIP] add auth cell

---
+++ ios/screens/SettingsViewController/SettingsAuthTableViewCell/SettingsAuthTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  SettingsAuthTableViewCell.m
+//  greenTravel
+//
+//  Created by Alex K on 31.01.23.
+//
+
+#import "SettingsAuthTableViewCell.h"
+
+@implementation SettingsAuthTableViewCell()
+
+@implementation SettingsAuthTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+}
+
+
+
+@end
