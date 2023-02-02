048fd5fd50ba646ea2e800aa87b9905495bcf824
nataliiayakorieva1 <nitoproekt@gmail.com>
2023-02-02 12:16:06 +0100

    Update AuthMethodSelection screen

--- src/screens/AuthMethodSelection/styles.ts
+++ src/screens/AuthMethodSelection/styles.ts
@@ -1,8 +1,30 @@
+import {COLORS} from 'assets';
+
 export const themeStyles = {
   container: {
     flex: 1,
     paddingHorizontal: 24,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  text: {
+    flexDirection: 'row',
+    textAlign: 'center',
+    position: 'absolute',
+    bottom: 8,
+    color: {
+      light: COLORS.boulder,
+      dark: COLORS.silver,
+    },
+  },
+  linkText: {
+    textDecorationLine: {
+      light: 'none',
+      dark: 'underline',
+    },
+    color: {
+      light: COLORS.cornflowerBlue,
+      dark: COLORS.silver,
+    },
   },
 };
