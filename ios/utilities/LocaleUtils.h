99e2127afcd84a7ed76d0ccd1aef967ec78869c4
Alexey Komov <alexeykcontact@gmail.com>
2023-02-13 20:38:35 +0300

    feat(638): use friendly names for language values

--- ios/utilities/LocaleUtils.h
+++ ios/utilities/LocaleUtils.h
@@ -10,6 +10,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 NSString* getCurrentLocaleLanguageCode(void);
+NSString* getCurrentLocaleFriendlyName(void);
 BOOL isCurrentLanguageCodeLegacy(void);
 
 NS_ASSUME_NONNULL_END
