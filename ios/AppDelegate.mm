615828bc7f13552851f32dba98af4561c52bc638
Aliaksei Astafyeu <lochost40@gmail.com>
2023-07-06 20:51:28 +0200

    add domains

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -120,5 +120,13 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   return [RCTLinkingManager application:application openURL:url options:options];
 }
 
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
+
 
 @end
