53c168015c6884036513ce99d6bbea527df8aacf
Aliaksei Astafyeu <lochost40@gmail.com>
2023-09-03 17:23:17 +0200

    fix: try to swap babel presets order

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
-    'module:metro-react-native-babel-preset',
     '@babel/preset-typescript',
+    'module:metro-react-native-babel-preset',
   ],
   plugins: [
     [
