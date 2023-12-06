8025bbfb7af21fcc85138a1c416794a9834ae7f3
Aliaksei Astafyeu <lochost40@gmail.com>
2023-12-06 21:22:23 +0100

    fix(fixed): fix issue on Android with map gestures

--- src/components/atoms/ClusterMap/ClusterMap.tsx
+++ src/components/atoms/ClusterMap/ClusterMap.tsx
@@ -121,8 +121,10 @@ export const ClusterMap = memo(
 
       return (
         <View
-          onResponderStart={onResponderStart}
-          onStartShouldSetResponder={() => true}
+          onStartShouldSetResponder={(event) => {
+            onResponderStart(event);
+            return false;
+          }}
           style={styles.container}
           {...getPlatformsTestID(testID)}>
           <MapView
