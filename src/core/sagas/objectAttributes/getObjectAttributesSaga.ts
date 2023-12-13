dc707d5ea9c77d99f5b9056d9d5dd0b28d3c0c2f
Vladislav Shevtsov <70779585+retater56@users.noreply.github.com>
2023-12-13 21:37:19 +0100

    feat(fixed): [567] condition for setPreparedVisitedObject

--- src/core/sagas/objectAttributes/getObjectAttributesSaga.ts
+++ src/core/sagas/objectAttributes/getObjectAttributesSaga.ts
@@ -1,15 +1,7 @@
-import {all, put, select} from 'redux-saga/effects';
+import {all} from 'redux-saga/effects';
 import {syncAndGetFavoritesSaga} from '../favorites/syncAndGetFavoritesSaga';
 import {getVisitedObjectsSaga} from '../visitedObjects/getVisitedObjectsSaga';
-import {selectPreparedVisitedObject} from 'core/selectors';
-import {addVisitedObjectRequest} from 'core/reducers';
 
 export function* getObjectAttributesSaga() {
-  const preparedVisitedObject = yield select(selectPreparedVisitedObject);
-
   yield all([syncAndGetFavoritesSaga(), getVisitedObjectsSaga()]);
-
-  if (preparedVisitedObject) {
-    yield put(addVisitedObjectRequest(preparedVisitedObject));
-  }
 }
