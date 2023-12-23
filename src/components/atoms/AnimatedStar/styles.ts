1581ee954dff0423f0e5ba4a7abed18cd682ac60
Vladislav Shevtsov <70779585+retater56@users.noreply.github.com>
2023-12-23 19:15:11 +0100

    feat(fixed): [634] ratings animation

--- src/components/atoms/AnimatedStar/styles.ts
+++ src/components/atoms/AnimatedStar/styles.ts
@@ -2,10 +2,6 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 5,
-  },
-  animationContainer: {
-    position: 'absolute',
-    zIndex: 1,
+    marginHorizontal: 10,
   },
 });
