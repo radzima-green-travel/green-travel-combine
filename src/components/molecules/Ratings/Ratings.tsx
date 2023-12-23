4c0884d1355213858698127313b9e1ceca50b8cf
Vladislav Shevtsov <70779585+retater56@users.noreply.github.com>
2023-12-23 17:13:35 +0100

    feat(fixed): [634] ratings animation

--- src/components/molecules/Ratings/Ratings.tsx
+++ src/components/molecules/Ratings/Ratings.tsx
@@ -3,7 +3,7 @@ import {View} from 'react-native';
 import {AnimatedStar} from 'atoms';
 import {map} from 'lodash';
 import {createNumericArray} from 'core/helpers';
-import {styles} from './srtyles';
+import {styles} from './styles';
 
 export const Ratings = () => {
   const [objectRating, setObjectRating] = useState(0);
@@ -15,7 +15,7 @@ export const Ratings = () => {
           key={String(point)}
           value={point}
           onPress={setObjectRating}
-          showAnimation={objectRating >= point}
+          marked={objectRating >= point}
         />
       ))}
     </View>
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {RefreshPageReminder, SuspenseView, SnackBar} from 'atoms';
+import {RefreshPageReminder, SuspenseView, SnackBar, AnimatedStar} from 'atoms';
 import {HomeSectionBar} from 'organisms';
 import {FlatList, RefreshControl, View} from 'react-native';
 
@@ -10,6 +10,7 @@ import {useHome} from './hooks';
 import {themeStyles} from './styles';
 import {TestIDs} from 'core/types';
 import {composeTestID} from 'core/helpers';
+import {Ratings} from 'molecules';
 
 export const Home = () => {
   const styles = useThemeStyles(themeStyles);
@@ -33,42 +34,43 @@ export const Home = () => {
   } = useHome();
 
   return (
-    <View style={styles.container}>
-      <SuspenseView
-        loading={loading}
-        error={homeData ? null : error}
-        retryCallback={getInitialData}>
-        <FlatList
-          ref={listRef}
-          style={styles.list}
-          contentContainerStyle={styles.contentContainer}
-          keyboardShouldPersistTaps="handled"
-          refreshControl={
-            <RefreshControl
-              tintColor={theme === 'light' ? COLORS.forestGreen : COLORS.white}
-              colors={[COLORS.forestGreen]}
-              refreshing={refreshing && isFocused}
-              onRefresh={getData}
-            />
-          }
-          data={homeData}
-          keyExtractor={item => item.id}
-          renderItem={({item, index}) => (
-            <HomeSectionBar
-              onObjectPress={navigateToObjectDetails}
-              onCategoryPress={onCategoryPress}
-              onAllObjectsPress={onAllObjectsPress}
-              onAllCategoriesPress={navigateToCategoriesList}
-              item={item}
-              allButtonTestID={composeTestID(TestIDs.AllButton, index)}
-              onObjectCardIsFavoriteChanged={sendIsFavoriteChangedEvent}
-            />
-          )}
-        />
-        {isUpdatesAvailable ? <RefreshPageReminder onPress={getData} /> : null}
-        <SnackBar isOnTop {...snackBarProps} />
-      </SuspenseView>
-    </View>
+    <Ratings />
+    // <View style={styles.container}>
+    //   <SuspenseView
+    //     loading={loading}
+    //     error={homeData ? null : error}
+    //     retryCallback={getInitialData}>
+    //     <FlatList
+    //       ref={listRef}
+    //       style={styles.list}
+    //       contentContainerStyle={styles.contentContainer}
+    //       keyboardShouldPersistTaps="handled"
+    //       refreshControl={
+    //         <RefreshControl
+    //           tintColor={theme === 'light' ? COLORS.forestGreen : COLORS.white}
+    //           colors={[COLORS.forestGreen]}
+    //           refreshing={refreshing && isFocused}
+    //           onRefresh={getData}
+    //         />
+    //       }
+    //       data={homeData}
+    //       keyExtractor={item => item.id}
+    //       renderItem={({item, index}) => (
+    //         <HomeSectionBar
+    //           onObjectPress={navigateToObjectDetails}
+    //           onCategoryPress={onCategoryPress}
+    //           onAllObjectsPress={onAllObjectsPress}
+    //           onAllCategoriesPress={navigateToCategoriesList}
+    //           item={item}
+    //           allButtonTestID={composeTestID(TestIDs.AllButton, index)}
+    //           onObjectCardIsFavoriteChanged={sendIsFavoriteChangedEvent}
+    //         />
+    //       )}
+    //     />
+    //     {isUpdatesAvailable ? <RefreshPageReminder onPress={getData} /> : null}
+    //     <SnackBar isOnTop {...snackBarProps} />
+    //   </SuspenseView>
+    // </View>
   );
 };
 
