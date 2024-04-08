93d308bf8ed202621330f17c9102060a83d8b114
Sergey Shulzhenko <serjconik@gmail.com>
2024-04-08 09:41:28 -0500

    fix(706): handle punctuation, refactor, fix type inference

--- src/core/helpers/common.ts
+++ src/core/helpers/common.ts
@@ -70,6 +70,11 @@ export const sanitizePhoneNumber = (phoneNumber: string) => {
   return `${plus}${digits}`;
 };
 
+export const removePunctuation = (input: string) => {
+  const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~«»]/g;
+  return input.replace(punctuation, '');
+};
+
 export function hexWithAlpha(hex: string, alpha: number): string {
   const r = parseInt(hex.slice(1, 3), 16);
   const g = parseInt(hex.slice(3, 5), 16);
