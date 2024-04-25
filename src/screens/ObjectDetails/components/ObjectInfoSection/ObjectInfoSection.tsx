9ba53201346412161510ff79c5682ee790c41421
Yauheni <HorbachZhenja@yandex.by>
2024-04-25 16:44:01 +0200

    feat(738): remove unnecessary parameters

--- src/screens/ObjectDetails/components/ObjectInfoSection/ObjectInfoSection.tsx
+++ src/screens/ObjectDetails/components/ObjectInfoSection/ObjectInfoSection.tsx
@@ -27,88 +27,74 @@ export type Item = {
 
 interface IProps {
   items: Item[];
-  priorityPosition?: ComponentProps<typeof ListItem>['position'];
-  containerListStyle?: StyleProp<ViewStyle>;
 }
 
-export const ObjectInfoSection = memo(
-  ({items, priorityPosition, containerListStyle}: IProps) => {
-    const styles = useThemeStyles(themeStyles);
-    const {t} = useTranslation('objectDetails');
-    const [isTitleTrunctated, setIsTitleTruncated] = useState(false);
+export const ObjectInfoSection = memo(({items}: IProps) => {
+  const styles = useThemeStyles(themeStyles);
+  const {t} = useTranslation('objectDetails');
+  const [isTitleTrunctated, setIsTitleTruncated] = useState(false);
 
-    const renderSectionItem = (item: Item, index: number) => {
-      const {
-        title = '',
-        subtitle = '',
-        onSubtitlePress,
-        leadIcon,
-        testID,
-        titleNumberOfLines,
-        withDropdown,
-        onPress,
-        contentStylingType = 'secondary',
-        boldTitle,
-        label = t('details'),
-        isAlwaysTrunctated,
-        onRightLabelPress,
-        containerStyle,
-        titleContainerStyle,
-      } = item;
+  const renderSectionItem = (item: Item, index: number) => {
+    const {
+      title = '',
+      subtitle = '',
+      onSubtitlePress,
+      leadIcon,
+      testID,
+      titleNumberOfLines,
+      withDropdown,
+      onPress,
+      contentStylingType = 'secondary',
+      boldTitle,
+      label = t('details'),
+      isAlwaysTrunctated,
+      onRightLabelPress,
+      containerStyle,
+      titleContainerStyle,
+    } = item;
 
-      const isNeedToTruncate = isTitleTrunctated || isAlwaysTrunctated;
+    const isNeedToTruncate = isTitleTrunctated || isAlwaysTrunctated;
 
-      let position: ComponentProps<typeof ListItem>['position'] = 'single';
+    let position: ComponentProps<typeof ListItem>['position'] = 'single';
 
-      if (items.length - 1 !== 0 && !priorityPosition) {
-        if (index === 0) {
-          position = 'top';
-        } else if (index === items.length - 1) {
-          position = 'bottom';
-        } else {
-          position = 'middle';
-        }
-      } else if (priorityPosition) {
-        if (index === items.length - 1) {
-          position = 'bottom';
-        } else {
-          position = priorityPosition;
-        }
+    if (items.length - 1 !== 0) {
+      if (index === 0) {
+        position = 'top';
+      } else if (index === items.length - 1) {
+        position = 'bottom';
+      } else {
+        position = 'middle';
       }
-
-      return (
-        <ListItem
-          containerStyle={[styles.listItemContainer, containerStyle]}
-          contentStylingType={contentStylingType}
-          title={title}
-          subtitle={subtitle}
-          onSubtitlePress={onSubtitlePress}
-          onRightLabelPress={onRightLabelPress}
-          leadIcon={leadIcon}
-          position={position}
-          leadIconStyle={styles.listItemIcon}
-          titleContainerStyle={titleContainerStyle}
-          testID={testID}
-          titleNumberOfLines={titleNumberOfLines}
-          tailIconStyle={withDropdown ? styles.dropdownTextStyle : undefined}
-          labelStyle={withDropdown ? styles.dropdownTextStyle : undefined}
-          tailIcon={
-            withDropdown && isNeedToTruncate ? 'chevronShortDown' : undefined
-          }
-          label={withDropdown && isNeedToTruncate ? label : undefined}
-          onPress={
-            withDropdown ? (isNeedToTruncate ? onPress : undefined) : onPress
-          }
-          boldTitle={boldTitle}
-          onTitleTruncate={withDropdown ? setIsTitleTruncated : undefined}
-        />
-      );
-    };
+    }
 
     return (
-      <View style={[styles.container, containerListStyle]}>
-        {items.map(renderSectionItem)}
-      </View>
+      <ListItem
+        containerStyle={[styles.listItemContainer, containerStyle]}
+        contentStylingType={contentStylingType}
+        title={title}
+        subtitle={subtitle}
+        onSubtitlePress={onSubtitlePress}
+        onRightLabelPress={onRightLabelPress}
+        leadIcon={leadIcon}
+        position={position}
+        leadIconStyle={styles.listItemIcon}
+        titleContainerStyle={titleContainerStyle}
+        testID={testID}
+        titleNumberOfLines={titleNumberOfLines}
+        tailIconStyle={withDropdown ? styles.dropdownTextStyle : undefined}
+        labelStyle={withDropdown ? styles.dropdownTextStyle : undefined}
+        tailIcon={
+          withDropdown && isNeedToTruncate ? 'chevronShortDown' : undefined
+        }
+        label={withDropdown && isNeedToTruncate ? label : undefined}
+        onPress={
+          withDropdown ? (isNeedToTruncate ? onPress : undefined) : onPress
+        }
+        boldTitle={boldTitle}
+        onTitleTruncate={withDropdown ? setIsTitleTruncated : undefined}
+      />
     );
-  },
-);
+  };
+
+  return <View style={[styles.container]}>{items.map(renderSectionItem)}</View>;
+});
