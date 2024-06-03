9964b87c63ab54cdf654fd210e5ec3cc6e6fa0a0
Aliaksei Astafyeu <lochost40@gmail.com>
2024-06-03 21:55:23 +0200

    Update app config to apply mapbox download token on CI

--- app.config.ts
+++ app.config.ts
@@ -7,6 +7,13 @@ export default ({config}: Dict) => {
     plugins: [
       ...(config.plugins ?? []),
       [withRemoveiOSNotificationEntitlement],
+      [
+        '@rnmapbox/maps',
+        {
+          RNMapboxMapsVersion: '11.1.0',
+          RNMapboxMapsDownloadToken: process.env.NETRC_PASSWORD ?? '',
+        },
+      ],
     ],
   };
 };
