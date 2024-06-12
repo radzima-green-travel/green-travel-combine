751334137f5e247fe40d036dc614f21467b8faee
Nataliia Yakorieva <82803292+NataliiaYakorieva@users.noreply.github.com>
2024-06-12 21:05:00 +0200

    fix(expo): fix amplitude (#799)

--- src/services/AnalyticsService.ts
+++ src/services/AnalyticsService.ts
@@ -1,31 +1,34 @@
-// import {Identify, Amplitude} from '@amplitude/react-native';
+import {
+  createInstance,
+  Types,
+  Identify,
+} from '@amplitude/analytics-react-native';
 import {Events, EventsPayload} from 'core/types';
 
 class AnalyticsService {
-  // instance: Amplitude;
+  instance: Types.ReactNativeClient;
 
   constructor() {
-    // this.instance = Amplitude.getInstance();
+    this.instance = createInstance();
   }
 
   init(apiKey: string) {
-    // this.instance.trackingSessionEvents(true);
-    // this.instance.init(apiKey);
-    // const identify = new Identify();
-    // identify.set('user_property_framework', 'react');
-    // this.instance.identify(identify);
+    this.instance.init(apiKey, undefined, {trackingSessionEvents: true});
+    const identify = new Identify();
+    identify.set('user_property_framework', 'react');
+    this.instance.identify(identify);
   }
 
   logEvent(
     //deprecated
     eventType: Events,
     params?: Record<string, any>,
-  ): Promise<boolean> {
-   // return this.instance.logEvent(eventType, params);
+  ): Types.AmplitudeReturn<Types.Result> {
+    return this.instance.logEvent(eventType, params);
   }
 
-  log({name, data}: EventsPayload): Promise<boolean> {
-    // return this.instance.logEvent(name, data);
+  log({name, data}: EventsPayload): Types.AmplitudeReturn<Types.Result> {
+    return this.instance.logEvent(name, data);
   }
 }
 
