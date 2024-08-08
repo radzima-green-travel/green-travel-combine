62e6e15a7c1cd2dfff5e65b719f060d6d89218ec
Aliaksei Astafyeu <lochost40@gmail.com>
2024-08-08 20:36:05 +0200

    upgrade expo to latest SDK

--- src/core/hooks/useObjectDetailsSelector.ts
+++ src/core/hooks/useObjectDetailsSelector.ts
@@ -1,15 +1,23 @@
 import {useCurrentDataSelector} from 'react-redux-help-kit';
-import {useRoute} from '@react-navigation/native';
+import {RouteProp, useRoute} from '@react-navigation/native';
+import {HomeNavigatorParamsList} from 'core/types';
+
+type ObjectDetailsScreenRouteProps = RouteProp<
+  HomeNavigatorParamsList,
+  'ObjectDetails'
+>;
 
 export const useObjectDetailsSelector = <
   T extends (state: any, id: string) => any,
 >(
   selector: T,
 ) => {
-  const {key} = useRoute();
+  const {
+    params: {objectId},
+  } = useRoute<ObjectDetailsScreenRouteProps>();
 
   return useCurrentDataSelector(selector, {
-    reducerId: key,
+    reducerId: objectId,
     reduxStateBranchName: 'objectDetails',
   });
 };
