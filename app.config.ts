c5cf9965b616a48640f0e1a770af584347b3115e
Aliaksei Astafyeu <lochost40@gmail.com>
2024-08-08 16:20:13 +0200

    try to get back expo updates

--- app.config.ts
+++ app.config.ts
@@ -4,12 +4,13 @@ type Dict = {[key: string]: any};
 export default ({config}: Dict) => {
   return {
     ...config,
-    // updates: {
-    //   url: 'https://u.expo.dev/e51feb59-6254-4001-bc5a-d7a5df40dbcb',
-    // },
-    // runtimeVersion: {
-    //   policy: 'appVersion',
-    // },
+    updates: {
+      enabled: false,
+      url: 'https://u.expo.dev/e51feb59-6254-4001-bc5a-d7a5df40dbcb',
+    },
+    runtimeVersion: {
+      policy: 'appVersion',
+    },
     name: process.env.APP_NAME ?? 'Radzima Dev',
     version: process.env.APP_VERSION ?? '1.10.0',
     ios: {
