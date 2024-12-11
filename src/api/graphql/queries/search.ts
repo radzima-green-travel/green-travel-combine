a04155e8afba915aafcbd481491d3db6a1812102
Aliaksei Astafyeu <lochost40@gmail.com>
2024-12-11 13:05:18 +0100

    fix query for search hostory

--- src/api/graphql/queries/search.ts
+++ src/api/graphql/queries/search.ts
@@ -86,7 +86,7 @@ export const getSearchObjectsQuery = (locale?: string) =>
   );
 
 export const getSearchObjectsHistoryQuery = `query MyQuery($match: String = "") {
-  searchObjects(limit: 15, filter: {id: {match: $match}, statuses: "published"}) {
+  searchObjects(limit: 15, filter: {id: {match: $match}, status: {eq: "published"}}) {
     items {
     ${searchObjectQueryParameters}
     }
