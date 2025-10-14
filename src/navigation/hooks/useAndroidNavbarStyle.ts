7a400e776c2b18f635ec81e56bfa287e909ca5cb
Andrii Syzov <54374036+albeitreluctantly@users.noreply.github.com>
2025-10-14 23:50:51 +0400

    chore: remove code not applicable to edge-to-edge mode

--- src/navigation/hooks/useAndroidNavbarStyle.ts
+++ src/navigation/hooks/useAndroidNavbarStyle.ts
@@ -2,7 +2,6 @@ import {useEffect} from 'react';
 import {isAndroid} from 'services/PlatformService';
 import * as NavigationBar from 'expo-navigation-bar';
 import {useColorScheme} from 'core/hooks';
-import {COLORS} from 'assets';
 
 export const useAndroidNavbarStyle = () => {
   const theme = useColorScheme();
@@ -10,10 +9,8 @@ export const useAndroidNavbarStyle = () => {
   useEffect(() => {
     if (isAndroid) {
       if (theme === 'light') {
-        NavigationBar.setBackgroundColorAsync(COLORS.light.background.tabBar);
         NavigationBar.setButtonStyleAsync('dark');
       } else {
-        NavigationBar.setBackgroundColorAsync(COLORS.dark.background.tabBar);
         NavigationBar.setButtonStyleAsync('light');
       }
     }
