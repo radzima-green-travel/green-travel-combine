902f32c251d5732ea8c7679908f0b2fd320e0133
Andrii Syzov <54374036+albeitreluctantly@users.noreply.github.com>
2026-03-05 16:54:49 +0400

    refactor(*): ObjectDetails header and status bar logic update (#958)

--- src/screens/ObjectDetails/hooks/useObjectDetailsStatusBar.ts
+++ src/screens/ObjectDetails/hooks/useObjectDetailsStatusBar.ts
@@ -1,5 +1,4 @@
 import { useColorScheme, useStatusBar } from 'core/hooks';
-import { useState } from 'react';
 import {
   SharedValue,
   runOnJS,
@@ -7,23 +6,17 @@ import {
 } from 'react-native-reanimated';
 import { IMAGE_HEIGHT } from '../styles';
 
-export function useObjectDetailsStatusBar(animatedValue: SharedValue<number>) {
-  const [isScrolledBelowImage, setIsScrolledBelowImage] = useState(false);
+export function useObjectDetailsStatusBar(scrollOffset: SharedValue<number>) {
   const theme = useColorScheme();
-
   const isLightTheme = theme === 'light';
 
-  useAnimatedReaction(
-    () => {
-      return animatedValue.value >= IMAGE_HEIGHT;
-    },
-    isScrollBelow => {
-      runOnJS(setIsScrolledBelowImage)(isScrollBelow);
-    },
-    [],
-  );
+  const { setStyle } = useStatusBar({ style: 'light' });
 
-  useStatusBar({
-    style: isLightTheme && isScrolledBelowImage ? 'dark' : 'light',
-  });
+  useAnimatedReaction(
+    () => scrollOffset.value >= IMAGE_HEIGHT,
+    isScrollBelow => {
+      runOnJS(setStyle)(isLightTheme && isScrollBelow ? 'dark' : 'light');
+    },
+    [isLightTheme],
+  );
 }
