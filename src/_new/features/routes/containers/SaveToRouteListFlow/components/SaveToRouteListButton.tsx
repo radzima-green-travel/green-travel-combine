861f9cedf37ebcc31ff8cc520560397ce7cfe920
Andrii Syzov <54374036+albeitreluctantly@users.noreply.github.com>
2026-03-15 01:01:40 +0400

    fix(*): highlight the button the object is added to a route

--- src/_new/features/routes/containers/SaveToRouteListFlow/components/SaveToRouteListButton.tsx
+++ src/_new/features/routes/containers/SaveToRouteListFlow/components/SaveToRouteListButton.tsx
@@ -1,15 +1,17 @@
+import { useValue } from '@legendapp/state/react';
 import { Button, Icon } from 'atoms';
 import { useContext } from 'react';
 import { SaveToRouteListFlowContext } from '../context';
 
 export const SaveToRouteListButton = () => {
-  const { menuProps } = useContext(SaveToRouteListFlowContext);
+  const { menuProps, state$ } = useContext(SaveToRouteListFlowContext);
+  const isActive = useValue(() => state$.initialRouteIds.get().size > 0);
 
   return (
     <Button
       testID="saveToRouteListButton"
       isIconOnlyButton
-      theme="secondary"
+      theme={isActive ? 'primary' : 'secondary'}
       onPress={menuProps.openMenu}
       renderIcon={textStyle => <Icon style={textStyle} name="addRoute" />}
     />
